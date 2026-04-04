ebe257ac3379335f3ec2b82066e8d5a90cc8d218
Your Name <you@example.com>
2026-04-04 09:39:39 +0530

    frontend init

---
+++ app/frontend/src/main.tsx
@@ -0,0 +1,31 @@
+import ReactDOM from "react-dom/client"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+
+import "./styles/global.css"
+import { routeTree } from "./routeTree.gen"
+import { ThemeProvider } from "./components/theme-provider.tsx"
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
+})
+
+// Register things for typesafety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById("app")!
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  )
+}
