14fa69a762a09af17471f0d027fbf0dc460a986c
Suven Pandey <pandeysuven@gmail.com>
2026-04-04 18:45:23 +0545

    fix: username is not unique

--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,6 @@ class User(BaseModel):
     class Meta:
         table_name = "users"
 
-    username = CharField(max_length=100, unique=True)
+    username = CharField(max_length=100, unique=False)
     email = CharField(max_length=255, unique=True)
     created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
